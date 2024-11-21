08af802e0b295e5e8f7406e4bbd3e4e289944ecd
ivoryeee <psa109@sookmyung.ac.kr>
2024-11-21 20:26:40 +0900

    init: 초기 프로젝트 세팅

---
+++ src/main/java/com/sopt/ios4/Ios4Application.java
@@ -0,0 +1,11 @@
+package com.sopt.ios4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ios4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Ios4Application.class, args);
+    }
+}
---
+++ src/test/java/com/sopt/ios4/Ios4ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sopt.ios4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Ios4ApplicationTests{
+    @Test
+    void contextLoads() {
+    }
+}
