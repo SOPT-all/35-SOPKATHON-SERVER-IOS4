e25deed9cc0f7a3d26501de0467d0a9ee6a04eda
choyeongju <jjudy1301@naver.com>
2024-11-24 06:24:56 +0900

    :sparkles: [feat] 변경사항 저장

--- src/main/java/com/sopt/ios4/controller/QuestionController.java
+++ src/main/java/com/sopt/ios4/controller/QuestionController.java
@@ -32,28 +32,27 @@ public class QuestionController {
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.CREATED.value(), "질문지가 생성되었습니다.", invitatinoCode));
     }
 
-    // 질문지 문제 풀기 -> 몇 개 맞았는지 반환
-    @PostMapping("/v1/questionnaire/test")
-    public ResponseEntity<ResponseDto<Integer>> solveQuestionElements(
-            @RequestParam long questionId,
-            @RequestBody AnswerListRequest answerListRequest
-a            )
-    {
-        // 질문지(Question) 테이블에서 questionId 에 해당하는 질문 찾아오기
-
-
-
-        int answerCount = 0; //맞은 개수 반환
-        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "요청이 성공했습니다.", answerCount));
-    }
 
     @GetMapping("/v1/questionnaire")
     public ResponseEntity<ResponseDto<List<String>>> createNewQuestion(@RequestParam int invitationCode
     ) {
         List<String> aa = newQuestionService.getQuestionsByInvitationCode((int) invitationCode);
-
-
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "질문지가 생성되었습니다.", aa));
     }
+
+//    // 질문지 문제 풀기 -> 몇 개 맞았는지 반환
+//    @PostMapping("/v1/questionnaire/test")
+//    public ResponseEntity<ResponseDto<Integer>> solveQuestionElements(
+//            @RequestParam long questionId,
+//            @RequestBody AnswerListRequest answerListRequest
+//    )
+//    {
+//        // 질문지(Question) 테이블에서 questionId 에 해당하는 질문 찾아오기
+//
+//
+//
+//        int answerCount = 0; //맞은 개수 반환
+//        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "요청이 성공했습니다.", answerCount));
+//    }
 }
 
