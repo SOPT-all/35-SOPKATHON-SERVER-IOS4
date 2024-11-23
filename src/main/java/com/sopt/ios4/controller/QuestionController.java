5d87008738cd6a93c22b1e9a32f62e068c7032ef
ivoryeee <psa109@sookmyung.ac.kr>
2024-11-24 06:17:48 +0900

    [feat] 초대코드 통해 질문지 조회하기

--- src/main/java/com/sopt/ios4/controller/QuestionController.java
+++ src/main/java/com/sopt/ios4/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.sopt.ios4.controller;
 
 import com.sopt.ios4.domain.NewQuestion;
+import com.sopt.ios4.domain.NewQuestionElement;
 import com.sopt.ios4.dto.ResponseDto;
 import com.sopt.ios4.dto.request.QuestionnarieCreateRequest;
 import com.sopt.ios4.service.question.NewQuestionService;
@@ -8,10 +9,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,4 +28,14 @@ public class QuestionController {
         int invitatinoCode = aa.getInvitationCode();
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.CREATED.value(), "질문지가 생성되었습니다.", invitatinoCode));
     }
+
+    @GetMapping("/v1/questionnaire")
+    public ResponseEntity<ResponseDto<List<String>>> createNewQuestion(@RequestParam int invitationCode
+    ) {
+        List<String> aa = newQuestionService.getQuestionsByInvitationCode((int) invitationCode);
+
+
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "질문지가 생성되었습니다.", aa));
+    }
 }
+
