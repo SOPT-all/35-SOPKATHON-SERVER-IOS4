e647b3be98a42dcbca222ee8047255a4744e4447
choyeongju <jjudy1301@naver.com>
2024-11-24 06:19:51 +0900

    :sparkles: [feat] 변경사항 저장

--- src/main/java/com/sopt/ios4/controller/QuestionController.java
+++ src/main/java/com/sopt/ios4/controller/QuestionController.java
@@ -2,16 +2,14 @@ package com.sopt.ios4.controller;
 
 import com.sopt.ios4.domain.NewQuestion;
 import com.sopt.ios4.dto.ResponseDto;
+import com.sopt.ios4.dto.request.AnswerListRequest;
 import com.sopt.ios4.dto.request.QuestionnarieCreateRequest;
 import com.sopt.ios4.service.question.NewQuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,11 +19,27 @@ public class QuestionController {
     private final NewQuestionService newQuestionService;
 
     @PostMapping("/v1/questionnaire")
-    public ResponseEntity<ResponseDto<Integer>> createNewQuestion(@RequestBody @Valid QuestionnarieCreateRequest newQuestion
-            ) {
-
+    public ResponseEntity<ResponseDto<Integer>> createNewQuestion(
+            @RequestBody @Valid QuestionnarieCreateRequest newQuestion
+            )
+    {
         NewQuestion aa = newQuestionService.createQuestionnaire(newQuestion);
         int invitatinoCode = aa.getInvitationCode();
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.CREATED.value(), "질문지가 생성되었습니다.", invitatinoCode));
+    }
+
+    // 질문지 문제 풀기 -> 몇 개 맞았는지 반환
+    @PostMapping("/v1/questionnaire/test")
+    public ResponseEntity<ResponseDto<Integer>> solveQuestionElements(
+            @RequestParam long questionId,
+            @RequestBody AnswerListRequest answerListRequest
+a            )
+    {
+        // 질문지(Question) 테이블에서 questionId 에 해당하는 질문 찾아오기
+
+
+
+        int answerCount = 0; //맞은 개수 반환
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "요청이 성공했습니다.", answerCount));
     }
 }
