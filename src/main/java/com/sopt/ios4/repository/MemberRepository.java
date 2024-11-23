67d0b4e5758ea4372deed71b4d44927eb53f9931
choyeongju <jjudy1301@naver.com>
2024-11-24 02:40:43 +0900

    :sparkles: [feat] 회원 가입 구현

--- src/main/java/com/sopt/ios4/repository/MemberRepository.java
+++ src/main/java/com/sopt/ios4/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.sopt.ios4.repository;
 
-public class MemberRepository {
+import com.sopt.ios4.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
