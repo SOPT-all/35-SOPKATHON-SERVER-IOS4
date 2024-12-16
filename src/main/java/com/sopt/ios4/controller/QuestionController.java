d8d744034248a14a246ce73ca821968cca75d971
ivoryeee <psa109@sookmyung.ac.kr>
2024-12-16 11:54:35 +0900

    [fix] 초대코드 통해 질문지 조회하기 응답값 questionId 추가

--- src/main/java/com/sopt/ios4/controller/QuestionController.java
+++ src/main/java/com/sopt/ios4/controller/QuestionController.java
@@ -3,7 +3,9 @@ package com.sopt.ios4.controller;
 import com.sopt.ios4.domain.NewQuestion;
 import com.sopt.ios4.domain.NewQuestionElement;
 import com.sopt.ios4.dto.ResponseDto;
+import com.sopt.ios4.dto.request.AnswerListRequest;
 import com.sopt.ios4.dto.request.QuestionnarieCreateRequest;
+import com.sopt.ios4.dto.response.QuestionnarieResponse;
 import com.sopt.ios4.service.question.NewQuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -30,12 +33,20 @@ public class QuestionController {
     }
 
     @GetMapping("/v1/questionnaire")
-    public ResponseEntity<ResponseDto<List<String>>> createNewQuestion(@RequestParam int invitationCode
+    public ResponseEntity<ResponseDto<QuestionnarieResponse>> createNewQuestion(@RequestParam int invitationCode
     ) {
-        List<String> aa = newQuestionService.getQuestionsByInvitationCode((int) invitationCode);
+        QuestionnarieResponse questionnarieResponse = newQuestionService.getQuestionsByInvitationCode(invitationCode);
+    return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "질문지가 생성되었습니다.", questionnarieResponse));
+    }
 
-
-        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "질문지가 생성되었습니다.", aa));
+    // 질문지 문제 풀기 -> 몇 개 맞았는지 반환
+    @PostMapping("/v1/questionnaire/test")
+    public ResponseEntity<ResponseDto<Integer>> solveQuestionElements(
+            @RequestParam long questionId,
+            @RequestBody AnswerListRequest answerListRequest
+    )
+    {
+        int answerCount = newQuestionService.getCounts(questionId, answerListRequest); // 몇 개 맞았는지 반환
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK.value(), "요청이 성공했습니다.", answerCount));
     }
 }
-
